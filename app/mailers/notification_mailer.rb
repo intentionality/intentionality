78716bf4027350586b754bf92163fe3a6b5252da
Winfred <winfred.nadeau@gmail.com>
2012-10-06 18:30:53 -0400

    apply mailchimp gem monkeypatch

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,3 +11,35 @@ class NotificationMailer < ActionMailer::Base
     mail to: todo.user.email
   end
 end
+
+#super monkey patch until https://github.com/mailchimp/mailchimp-gem/pull/7
+module Mailchimp
+  class MandrillDeliveryHandler
+    def deliver!(message)
+      
+      message_payload = {
+        :track_opens => settings[:track_opens],
+        :track_clicks => settings[:track_clicks],
+        :message => {
+          :subject => message.subject,
+          :from_name => settings[:from_name],
+          :from_email => message.from.first,
+          :to => message.to.map{|m|{email: m}}
+        }
+      }
+
+      [:html, :text].each do |format|
+        content = get_content_for(message, format)
+        message_payload[:message][format] = content if content
+      end
+
+      message_payload[:tags] = settings[:tags] if settings[:tags]
+      
+      api_key = message.header['api-key'].blank? ? settings[:api_key] : message.header['api-key']
+      
+      resp = Mailchimp::Mandrill.new(api_key).messages_send(message_payload)
+      Logger.new($STDOUT).info resp.inspect
+      
+    end
+  end
+end
