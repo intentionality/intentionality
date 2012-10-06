6869ecdda4545073f655b5d6c9c4b47a4858bcd8
Winfred <winfred.nadeau@gmail.com>
2012-10-06 10:34:28 -0400

    add active_record session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Intentionality::Application.config.session_store :cookie_store, key: '_intentionality_session'
+Intentionality::Application.config.session_store :active_record_store, key: '_intentionality_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
