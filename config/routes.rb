b87dfd383f0a184c283e048bf5a0925a6c02276b
Winfred <winfred.nadeau@gmail.com>
2012-10-06 10:27:07 -0400

    add static page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Intentionality::Application.routes.draw do
   resources :todos
 
+  root to: 'static#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
